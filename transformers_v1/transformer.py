035b975bc281e637c3dd76b8616df0a3ce80710f
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-02 11:33:12 +0530

    added mask

--- transformers_v1/transformer.py
+++ transformers_v1/transformer.py
@@ -1,4 +1,11 @@
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
-print(classifier("I've been waiting for a HuggingFace course my whole life."))
+# classifier = pipeline("sentiment-analysis")
+# print(classifier("I've been waiting for a HuggingFace course my whole life."))
+
+unmasker = pipeline("fill-mask", model="bert-base-uncased")
+result = unmasker("This man works as a [MASK].")
+print([r["token_str"] for r in result])
+
+result = unmasker("This woman works as a [MASK].")
+print([r["token_str"] for r in result])
