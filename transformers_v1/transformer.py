f0788609774309b737974d3343d15a45b6cad17f
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-03 11:52:46 +0530

    loading pretrainded model

--- transformers_v1/transformer.py
+++ transformers_v1/transformer.py
@@ -1,4 +1,6 @@
 from transformers import pipeline
+from transformers import BertConfig, BertModel
+
 
 # classifier = pipeline("sentiment-analysis")
 # print(classifier("I've been waiting for a HuggingFace course my whole life."))
@@ -9,3 +11,8 @@ print([r["token_str"] for r in result])
 
 result = unmasker("This woman works as a [MASK].")
 print([r["token_str"] for r in result])
+
+config = BertConfig()
+model = BertModel(config)
+
+print(config)
