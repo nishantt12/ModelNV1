942e340664a7b4cf83cf73449a478836c19be8fe
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-04 12:32:02 +0530

    working with tokens

--- transformers_v1/transformer.py
+++ transformers_v1/transformer.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
 from transformers import BertConfig, BertModel
+from transformers import AutoTokenizer
 
 
 # classifier = pipeline("sentiment-analysis")
@@ -7,12 +8,22 @@ from transformers import BertConfig, BertModel
 
 unmasker = pipeline("fill-mask", model="bert-base-uncased")
 result = unmasker("This man works as a [MASK].")
-print([r["token_str"] for r in result])
+# print([r["token_str"] for r in result])
 
 result = unmasker("This woman works as a [MASK].")
-print([r["token_str"] for r in result])
+# print([r["token_str"] for r in result])
 
 config = BertConfig()
 model = BertModel(config)
 
-print(config)
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+sequence = "Using a Transformer network is simple"
+tokens = tokenizer.tokenize(sequence)
+
+ids = tokenizer.convert_tokens_to_ids(tokens)
+
+decoded_string = tokenizer.decode(ids)
+
+print(decoded_string)
