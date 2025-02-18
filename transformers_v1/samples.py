3ac925f13c9f4853846c5f6057cd4a1875e13c69
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-18 14:29:19 +0530

    using dataset for github issues

--- transformers_v1/samples.py
+++ transformers_v1/samples.py
@@ -1,5 +1,16 @@
 from datasets import load_dataset
 
-ds = load_dataset("aidanzhou/tinderflirting")
+issues_dataset = load_dataset("lewtun/github-issues", split="train")
+issues_dataset = issues_dataset.filter(
+    lambda x: (x["is_pull_request"] == False and len(x["comments"]) > 0)
+)
 
-print(ds['train'][:5])
+columns = issues_dataset.column_names
+columns_to_keep = ["title", "body", "html_url", "comments"]
+columns_to_remove = set(columns_to_keep).symmetric_difference(columns)
+issues_dataset = issues_dataset.remove_columns(columns_to_remove)
+issues_dataset.set_format("pandas")
+df = issues_dataset[:]
+comments_df = df.explode("comments", ignore_index=True)
+comments_df.head(4)
+print(comments_df)
