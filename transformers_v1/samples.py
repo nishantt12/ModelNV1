74bce1328a37272f9f26999dbdd862e10392e8a8
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-19 16:29:57 +0530

    token classification

--- transformers_v1/samples.py
+++ transformers_v1/samples.py
@@ -1,16 +1,6 @@
 from datasets import load_dataset
 
-issues_dataset = load_dataset("lewtun/github-issues", split="train")
-issues_dataset = issues_dataset.filter(
-    lambda x: (x["is_pull_request"] == False and len(x["comments"]) > 0)
-)
+raw_datasets = load_dataset("conll2003", trust_remote_code=True)
 
-columns = issues_dataset.column_names
-columns_to_keep = ["title", "body", "html_url", "comments"]
-columns_to_remove = set(columns_to_keep).symmetric_difference(columns)
-issues_dataset = issues_dataset.remove_columns(columns_to_remove)
-issues_dataset.set_format("pandas")
-df = issues_dataset[:]
-comments_df = df.explode("comments", ignore_index=True)
-comments_df.head(4)
-print(comments_df)
+print(raw_datasets["train"][0])
+print(raw_datasets["train"][0]["tokens"])
