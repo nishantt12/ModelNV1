1361bd4b712a5454a2bec9a8eb67b5999125c220
Nishant Tanwar <nishant.tanwar@tala.co>
2025-02-20 13:11:06 +0530

    testing ing

--- transformers_v1/samples.py
+++ transformers_v1/samples.py
@@ -1,23 +1,6 @@
-# from datasets import load_dataset
-#
-# raw_datasets = load_dataset("conll2003", trust_remote_code=True)
-#
-# print(raw_datasets["train"][0])
-# print(raw_datasets["train"][0]["tokens"])
+from datasets import load_dataset
 
-# from transformers import pipeline
-#
-# model_checkpoint = "Helsinki-NLP/opus-mt-en-fr"
-# translator = pipeline("translation", model=model_checkpoint)
-# print(translator("Default to expanded threads"))
+raw_datasets = load_dataset("conll2003", trust_remote_code=True)
 
-import gradio as gr
-
-
-def greet(name):
-    return "Hello " + name
-
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-
-demo.launch()
+print(raw_datasets["train"][0])
+print(raw_datasets["train"][0]["tokens"])
